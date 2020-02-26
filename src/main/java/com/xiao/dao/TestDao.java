534ff0b1f3f12a65e94606e579712b48824c3350
xiaobo0528n <xiaobo0528n@gmail.com>
2020-02-26 18:51:42 +0800

    InitEnvironment

---
+++ src/main/java/com/xiao/dao/TestDao.java
@@ -0,0 +1,14 @@
+package com.xiao.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author xiaobo
+ * @date 2020/2/26 18:51
+ * @since 1.0
+ */
+
+@Repository("testDao")
+public interface TestDao {
+
+}
